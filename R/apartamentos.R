afd0e293a9348c31b7635a5a8703192accdf4948
Daniel E Gonzalez <dgonxalex80@gmail.com>
2023-03-26 21:38:00 -0500

    ajuses tutoriales

---
+++ R/apartamentos.R
@@ -0,0 +1,11 @@
+#' Datos del precio de 141 apartamento con su área 
+#'
+#' La base corresponde a datos sobre 141 apartamentos para fines academicos. 
+#'
+#' @format Base de datos con 141 filas y 2 variables:
+#' \describe{
+#'   \item{precio}{precio del apartamento en millones de pesos}
+#'   \item{area.cons}{área del apartamento}
+#' }
+#' @source 
+"apartamentos"
---
+++ R/creditos.R
@@ -0,0 +1,32 @@
+#' Datos del precio de 780 clientes de un banco 
+#'
+#' La base corresponde a datos sobre 780 clientes de un banco con 5 variables. 
+#'
+
+#' @source 
+"creditos"
+
+#' Iris Data Set
+#'
+#' 
+#'
+#' @details
+#' La base contiene informacion sobre el comportamiento de 780 clientes de un banco y sus principales caracteristicas.
+#'
+#' @source base creada para fines académicos.
+#'
+#' @format base  con 780 filas y 5 variables:
+#' \describe{
+#'   \item{default}{tiene el valor de 1 si el cliente no ha pagado y 0 si ha pagado su crédito}
+#'   \item{antiguedad}{antiguedad del cliente en el banco}
+#'   \item{edad}{edad del ciente}
+#'   \item{cuota}{cuota del crédito que debe pagar}
+#'   \item{ingresos}{ingresos del cliente}
+#' }
+#'
+#' @examples
+#' data(creditos)
+#' summary(creditos)
+#'
+#' @keywords datasets
+"creditos"
