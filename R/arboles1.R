be44e119348faca534773a5a4c6036bde1565f6b
Daniel E Gonzalez <dgonxalex80@gmail.com>
2023-04-03 22:32:30 -0500

    add arboles1.r

---
+++ R/arboles1.R
@@ -0,0 +1,14 @@
+#' Datos de 90 árboles 
+#'
+#' La base corresponde a datos sobre 90 árboles  
+#'
+#' @format Base de datos con 141 filas y 2 variables:
+#' \describe{
+#'   \item{finca}{ubicación del árbol}
+#'   \item{mg}{genotipo del árbol}
+#'   \item{preso}{peso del árbol en ton}
+#'   \item{diametro}{diametro del árbol}
+#'   \item{altura}{altura del árbol}
+#' }
+#' @source 
+"arboles1"
