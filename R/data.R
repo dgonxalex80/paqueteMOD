16763391ae2de20832c2832e78f824e5f5177421
Jose Francisco Botero <61252132+josebrx99@users.noreply.github.com>
2023-04-29 11:19:23 -0500

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' animals
+#'
+#' Base de datos con cinco componentes de la leche materna de 25 especies de animales.
+#'
+#' @format Un objeto de tipo data.frame.
+#' @name animals
+#' @docType data
+#' \describe{
+#'   \item{water}{Porcentaje de agua que compone la leche materna.}
+#'   \item{protein}{Porcentaje de agua que compone la leche materna.}
+#'   \item{fat}{Porcentaje de grasa que compone la leche materna.}
+#'   \item{lactose}{Porcentaje de lactosa que compone la leche materna.}
+#'   \item{ash}{Porcentaje de ceniza que compone la leche materna.}
+#' }
+#'
+"animals"
+
+
+
+p = paqueteMOD::animals
+View(p)
+str(p)
