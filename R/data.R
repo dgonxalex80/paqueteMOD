83a69f38f0ec22fdb23b8a56d387838737af1423
Jose Francisco Botero <61252132+josebrx99@users.noreply.github.com>
2023-04-29 11:08:01 -0500

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' Title for dataset 1
+#'
+#' description for dataset1
+#'
+#' @name dataset1
+#' @keywords dataset1
+#' @docType data
+#' @format An S4 object of class RasterLayer.
+#' @details
+#'     details for dataset1
+#'
+"animals"
+
+#' Title for dataset 2
+#'
+#' description for dataset1
+#'
+#' @format An S4 object of class SpatialPolygonsDataFrame.
+#' @name dataset2
+#' @docType data
+#' @details
+#'     details for dataset2
+#' \describe{
+#'   \item{name_column_1}{description of column1}
+#'   \item{name_column_1}{description of column1}
+#' }
+#'
+"apartamentos"
